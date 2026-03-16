29e3f8f41c7285fe360cf9d14ad85b488b138fc4
sharp-lilac <jessicacboles@gmail.com>
2026-03-16 15:55:40 -0600

    Update packages_attach.r

--- packages_attach.r
+++ packages_attach.r
@@ -1,1 +1,19 @@
 # packages_attach.r
+
+## Check required packages ------------------------
+options(repos = c(CRAN = "https://cran.rstudio.com/"))
+required_packages <- c("httr2", "jsonlite", "rcrossref", "dplyr", "readr", "stringr")
+install_if_missing <- function(package) {
+    if (!requireNamespace(package, quietly = TRUE)) {
+        install.packages(package)
+    }
+}
+invisible(lapply(required_packages, install_if_missing))
+
+## Attach packages ------------------------
+library(httr2)
+library(jsonlite)
+library(rcrossref)
+library(dplyr)
+library(readr)
+library(stringr)
